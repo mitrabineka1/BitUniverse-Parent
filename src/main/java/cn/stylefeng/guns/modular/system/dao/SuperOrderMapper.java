d2e0c90b7390a74fef45707d39e943a3128c8f5d
sdzczh <zgsdzczh@163.com>
2019-01-09 15:45:47 +0800

    commit

--- src/main/java/cn/stylefeng/guns/modular/system/dao/SuperOrderMapper.java
+++ src/main/java/cn/stylefeng/guns/modular/system/dao/SuperOrderMapper.java
@@ -3,6 +3,7 @@ package cn.stylefeng.guns.modular.system.dao;
 import cn.stylefeng.guns.modular.system.model.SuperOrder;
 import com.baomidou.mybatisplus.mapper.BaseMapper;
 import com.baomidou.mybatisplus.plugins.Page;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 import java.util.Map;
@@ -17,5 +18,5 @@ import java.util.Map;
  */
 public interface SuperOrderMapper extends BaseMapper<SuperOrder> {
 
-    List<Map<String, Object>> selectLists(String coinId, Page<SuperOrder> page);
+    List<Map<String, Object>> selectLists(@Param("coin") String coin, @Param("page") Page<SuperOrder> page);
 }
