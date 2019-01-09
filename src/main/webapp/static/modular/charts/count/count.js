4a590ae6fe38eebfd18e963b796cb5b797fb1675
sdzczh <zgsdzczh@163.com>
2019-01-09 16:50:33 +0800

    commit

--- src/main/webapp/static/modular/charts/count/count.js
+++ src/main/webapp/static/modular/charts/count/count.js
@@ -95,3 +95,490 @@ $(function () {
     table.setPaginationType("client");
     Count.table = table.init();
 });
+
+
+function init(c2, gear) {
+    var myChart = echarts.init(document.getElementById('main')); //渲染组件
+    myChart.showLoading({ //加载动画
+        text: '正在加载数据。。。。'
+    });
+
+    //加载数据
+    jQuery.ajax({
+        url: "count/kline",
+        type: 'get',
+        data: {coinId: c2, exchangeId: 1, gear: gear},
+        datatype: 'json',
+        async: true,
+        success: function (jsons) {
+
+            if (jsons) {
+                myChart.hideLoading(); //隐藏动画
+                // 数据意义：开盘(open)，收盘(close)，最低(lowest)，最高(highest)
+                var data0 = splitData(jsons);
+
+                //切割数组，把数组中的日期和数据分离，返回数组中的日期和数据
+                function splitData(rawData) {
+                    var categoryData = [];
+                    var values = [];
+                    rawData = JSON.parse(rawData);
+                    console.info(rawData);
+                    for (var i = 0; i < rawData.length; i++) {
+                        //splice 返回每组数组中被删除的第一项，即返回数组中被删除的日期
+                        //console.info(rawData[i].splice(0, 1)[0]);
+                        //categoryData  日期  把返回的日期放到categoryData[]数组中
+                        categoryData.push(rawData[i].splice(0, 1)[0]);
+                        //alert(categoryData);
+
+                        //数据数组，即数组中除日期外的数据
+                        // alert(rawData[i]);
+                        values.push(rawData[i])
+                    }
+                    return {
+                        categoryData: categoryData, //数组中的日期 x轴对应的日期
+                        values: values              //数组中的数据 y轴对应的数据
+                    };
+                }
+
+                //计算MA平均线，N日移动平均线=N日收盘价之和/N  dayCount要计算的天数(5,10,20,30)
+                function calculateMA(dayCount) {
+                    var result = [];
+                    for (var i = 0, len = data0.values.length; i < len; i++) {
+                        if (i < dayCount) {
+                            result.push('-');
+                            //alert(result);
+                            continue;   //结束单次循环，即不输出本次结果
+                        }
+                        var sum = 0;
+                        for (var j = 0; j < dayCount; j++) {
+                            //收盘价总和
+                            sum += data0.values[i - j][1];
+                            //alert(sum);
+                        }
+                        result.push(sum / dayCount);
+                        // alert(result);
+                    }
+                    return result;
+                }
+
+                option = {
+                    title: {    //标题
+                        text: '价格走势图',
+                        left: 0
+                    },
+                    tooltip: {  //提示框
+                        trigger: 'axis',    //触发类型：坐标轴触发
+                        axisPointer: {  //坐标轴指示器配置项
+                            type: 'cross'   //指示器类型，十字准星
+                        }
+                    },
+                    legend: {   //图例控件，点击图例控制哪些系列不现实
+                        data: ['日K', 'MA5', 'MA10', 'MA20', 'MA30']
+                    },
+                    grid: {     //直角坐标系
+                        show: true,
+                        left: '10%',    //grid组件离容器左侧的距离
+                        right: '10%',
+                        bottom: '15%',
+                        //backgroundColor:'#ccc'
+                    },
+                    xAxis: {
+                        type: 'category',   //坐标轴类型，类目轴
+                        data: data0.categoryData,
+                        //scale: true,  //只在数字轴中有效
+                        boundaryGap: false,    //刻度作为分割线，标签和数据点会在两个刻度上
+                        axisLine: {onZero: false},
+                        splitLine: {show: false},   //是否显示坐标轴轴线
+                        //splitNumber: 20,    //坐标轴的分割段数，预估值，在类目轴中无效
+                        min: 'dataMin', //特殊值，数轴上的最小值作为最小刻度
+                        max: 'dataMax'  //特殊值，数轴上的最大值作为最大刻度
+                    },
+                    yAxis: {
+                        scale: true,    //坐标刻度不强制包含零刻度
+                        splitArea: {
+                            show: true  //显示分割区域
+                        }
+                    },
+                    dataZoom: [     //用于区域缩放
+                        {
+                            filterMode: 'filter',    //当前数据窗口外的数据被过滤掉来达到数据窗口缩放的效果  默认值filter
+                            type: 'inside', //内置型数据区域缩放组件
+                            start: 50,      //数据窗口范围的起始百分比
+                            end: 100        //数据窗口范围的结束百分比
+                        },
+                        {
+                            show: true,
+                            type: 'slider', //滑动条型数据区域缩放组件
+                            y: '90%',
+                            start: 50,
+                            end: 100
+                        }
+                    ],
+                    series: [   //图表类型
+                        {
+                            name: '日K',
+                            type: 'candlestick',    //K线图
+                            data: data0.values,     //y轴对应的数据
+
+                            ////////////////////////图标标注/////////////////////////////
+
+                            markPoint: {    //图表标注
+                                label: {    //标注的文本
+                                    normal: {   //默认不显示标注
+                                        show: true,
+                                        //position:['20%','30%'],
+                                        formatter: function (param) {   //标签内容控制器
+                                            return param != null ? Math.round(param.value) : '';
+                                        }
+                                    }
+                                },
+                                data: [     //标注的数据数组
+                                    {
+                                        name: 'XX标点',
+                                        coord: ['2013/5/31', 2300], //指定数据的坐标位置
+                                        value: 2300,
+                                        itemStyle: {    //图形样式
+                                            normal: {color: 'rgb(41,60,85)'}
+                                        }
+                                    },
+                                    {
+                                        name: 'highest value',
+                                        type: 'max',    //最大值
+                                        valueDim: 'highest'     //在highest维度上的最大值 最高价
+                                    },
+                                    {
+                                        name: 'lowest value',
+                                        type: 'min',
+                                        valueDim: 'lowest'  //最低价
+                                    },
+                                    {
+                                        name: 'average value on close',
+                                        type: 'average',
+                                        valueDim: 'close'   //收盘价
+                                    }
+                                ],
+                                tooltip: {      //提示框
+                                    formatter: function (param) {
+                                        return param.name + '<br>' + (param.data.coord || '');
+                                    }
+                                }
+                            },
+
+/////////////////////////////////图标标线///////////////////////////
+
+                            markLine: {
+                                symbol: ['none', 'none'],   //标线两端的标记类型
+                                data: [
+                                    [
+                                        {
+                                            name: 'from lowest to highest',
+                                            type: 'min',    //设置该标线为最小值的线
+                                            valueDim: 'lowest', //指定在哪个维度上的最小值
+                                            symbol: 'circle',
+                                            symbolSize: 10, //起点标记的大小
+                                            label: {    //normal默认，emphasis高亮
+                                                normal: {show: false},  //不显示标签
+                                                emphasis: {show: false} //不显示标签
+                                            }
+                                        },
+                                        {
+                                            type: 'max',
+                                            valueDim: 'highest',
+                                            symbol: 'circle',
+                                            symbolSize: 10,
+                                            label: {
+                                                normal: {show: false},
+                                                emphasis: {show: false}
+                                            }
+                                        }
+                                    ],
+
+                                    {
+                                        name: 'min line on close',
+                                        type: 'min',
+                                        valueDim: 'close'
+                                    },
+                                    {
+                                        name: 'max line on close',
+                                        type: 'max',
+                                        valueDim: 'close'
+                                    }
+                                ]
+
+                            }
+
+                        },
+
+                        {   //MA5 5天内的收盘价之和/5
+                            name: 'MA5',
+                            type: 'line',
+                            data: calculateMA(5),
+                            smooth: true,
+                            lineStyle: {
+                                normal: {opacity: 0.5}
+                            }
+                        },
+                        {
+                            name: 'MA10',
+                            type: 'line',
+                            data: calculateMA(10),
+                            smooth: true,
+                            lineStyle: {    //标线的样式
+                                normal: {opacity: 0.5}
+                            }
+                        },
+                        {
+                            name: 'MA20',
+                            type: 'line',
+                            data: calculateMA(20),
+                            smooth: true,
+                            lineStyle: {
+                                normal: {opacity: 0.5}
+                            }
+                        },
+                        {
+                            name: 'MA30',
+                            type: 'line',
+                            data: calculateMA(30),
+                            smooth: true,
+                            lineStyle: {
+                                normal: {opacity: 0.5}
+                            }
+                        },
+
+                    ]
+                };
+                myChart.setOption(option);
+            }
+        },
+        error: function () {
+            alert("数据加载失败！请检查数据链接是否正确");
+        }
+    });
+}
+
+function detail(){
+    var myChart = echarts.init(document.getElementById('main'));
+    var builderJson = {
+        "all": 119,
+        "charts": {
+            "软件开发":27,
+            "技术工程师":22,
+            "其他":22,
+            "教师":21,
+            "JAVA":12,
+            "工程师":4,
+            "Android":2,
+            "后端开发":2,
+            "前端开发":2,
+            "数据库开发":2,
+            ".net":1,
+            "算法":1,
+            "游戏策划":1,
+            "运营":1
+        },
+        "components": {
+            "教师":32,
+            "软件开发":21,
+            "其他":16,
+            "测试":15,
+            "技术工程师":14,
+            "产品":7,
+            "安卓":2,
+            "JAVA":2,
+            "工程师":2,
+            "数据分析":2,
+            "硬件工程师":2,
+            "VR":1,
+            "后端开发":1,
+            "运营":1
+
+        },
+        "ie": 9743
+    };
+
+    var downloadJson = {
+        "echarts.min.js": 17365,
+        "echarts.simple.min.js": 4079,
+        "echarts.common.min.js": 6929,
+        "echarts.js": 14890
+    };
+
+    var themeJson = {
+        "dark.js": 1594,
+        "infographic.js": 925,
+        "shine.js": 1608,
+        "roma.js": 721,
+        "macarons.js": 2179,
+        "vintage.js": 1982,
+        "1" :111
+    };
+
+    var waterMarkText = '';
+
+    var canvas = document.createElement('canvas');
+    var ctx = canvas.getContext('2d');
+    canvas.width = canvas.height = 100;
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle';
+    ctx.globalAlpha = 0.08;
+    ctx.font = '20px Microsoft Yahei';
+    ctx.translate(50, 50);
+    ctx.rotate(-Math.PI / 4);
+    ctx.fillText(waterMarkText, 0, 0);
+
+    option = {
+        backgroundColor: {
+            type: 'pattern',
+            image: canvas,
+            repeat: 'repeat'
+        },
+        tooltip: {},
+        title: [{
+            text: '在线构建',
+            subtext: '总计 ' + builderJson.all,
+            x: '25%',
+            textAlign: 'center'
+        }, {
+            text: '各版本下载',
+            subtext: '总计 ' + Object.keys(downloadJson).reduce(function (all, key) {
+                return all + downloadJson[key];
+            }, 0),
+            x: '75%',
+            textAlign: 'center'
+        }, {
+            text: '主题下载',
+            subtext: '总计 ' + Object.keys(themeJson).reduce(function (all, key) {
+                return all + themeJson[key];
+            }, 0),
+            x: '75%',
+            y: '50%',
+            textAlign: 'center'
+        }],
+        grid: [{
+            top: 50,
+            width: '50%',
+            bottom: '45%',
+            left: 10,
+            containLabel: true
+        }, {
+            top: '55%',
+            width: '50%',
+            bottom: 0,
+            left: 10,
+            containLabel: true
+        }],
+        xAxis: [{
+            type: 'value',
+            max: builderJson.all,
+            splitLine: {
+                show: false
+            }
+        }, {
+            type: 'value',
+            max: builderJson.all,
+            gridIndex: 1,
+            splitLine: {
+                show: false
+            }
+        }],
+        yAxis: [{
+            type: 'category',
+            data: Object.keys(builderJson.charts),
+            axisLabel: {
+                interval: 0,
+                rotate: 30
+            },
+            splitLine: {
+                show: false
+            }
+        }, {
+            gridIndex: 1,
+            type: 'category',
+            data: Object.keys(builderJson.components),
+            axisLabel: {
+                interval: 0,
+                rotate: 30
+            },
+            splitLine: {
+                show: false
+            }
+        }],
+        series: [{
+            type: 'bar',
+            stack: 'chart',
+            z: 3,
+            label: {
+                normal: {
+                    position: 'right',
+                    show: true
+                }
+            },
+            data: Object.keys(builderJson.charts).map(function (key) {
+                return builderJson.charts[key];
+            })
+        }, {
+            type: 'bar',
+            stack: 'chart',
+            silent: true,
+            itemStyle: {
+                normal: {
+                    color: '#eee'
+                }
+            },
+            data: Object.keys(builderJson.charts).map(function (key) {
+                return builderJson.all - builderJson.charts[key];
+            })
+        }, {
+            type: 'bar',
+            stack: 'component',
+            xAxisIndex: 1,
+            yAxisIndex: 1,
+            z: 3,
+            label: {
+                normal: {
+                    position: 'right',
+                    show: true
+                }
+            },
+            data: Object.keys(builderJson.components).map(function (key) {
+                return builderJson.components[key];
+            })
+        }, {
+            type: 'bar',
+            stack: 'component',
+            silent: true,
+            xAxisIndex: 1,
+            yAxisIndex: 1,
+            itemStyle: {
+                normal: {
+                    color: '#eee'
+                }
+            },
+            data: Object.keys(builderJson.components).map(function (key) {
+                return builderJson.all - builderJson.components[key];
+            })
+        }, {
+            type: 'pie',
+            radius: [0, '30%'],
+            center: ['75%', '25%'],
+            data: Object.keys(downloadJson).map(function (key) {
+                return {
+                    name: key.replace('.js', ''),
+                    value: downloadJson[key]
+                }
+            })
+        }, {
+            type: 'pie',
+            radius: [0, '30%'],
+            center: ['75%', '75%'],
+            data: Object.keys(themeJson).map(function (key) {
+                return {
+                    name: key.replace('.js', ''),
+                    value: themeJson[key]
+                }
+            })
+        }]
+    };
+    myChart.setOption(option);
+}
