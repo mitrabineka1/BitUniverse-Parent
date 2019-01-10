851cb5deec2779b3c29a057b660e04ed26b62b8c
sdzczh <zgsdzczh@163.com>
2019-01-10 12:01:17 +0800

    commit

--- src/main/webapp/static/modular/count/funds/funds.js
+++ src/main/webapp/static/modular/count/funds/funds.js
@@ -95,3 +95,208 @@ $(function () {
     table.setPaginationType("client");
     Funds.table = table.init();
 });
+
+function detail(c2) {
+    var myChart = echarts.init(document.getElementById('main2')); //渲染组件
+    myChart.showLoading({ //加载动画
+        text: '正在加载数据。。。。'
+    });
+
+    //加载数据
+    jQuery.ajax({
+        url: "funds/fundDIs",
+        type: 'get',
+        data: {coinId: c2},
+        datatype: 'json',
+        async: true,
+        success: function (jsons) {
+
+            if (jsons) {
+                myChart.hideLoading(); //隐藏动画
+                jsons = JSON.parse(jsons);
+                console.info(jsons);
+                var builderJson = {
+                    "all": 7000000000,
+                    "charts": jsons.buy,
+                    "components": jsons.sale,
+                    "ie": 9743
+                };
+
+                var downloadJson = jsons.buy;
+
+                var themeJson = jsons.sale;
+
+                var waterMarkText = '';
+
+                var canvas = document.createElement('canvas');
+                var ctx = canvas.getContext('2d');
+                canvas.width = canvas.height = 100;
+                ctx.textAlign = 'center';
+                ctx.textBaseline = 'middle';
+                ctx.globalAlpha = 0.08;
+                ctx.font = '20px Microsoft Yahei';
+                ctx.translate(50, 50);
+                ctx.rotate(-Math.PI / 4);
+                ctx.fillText(waterMarkText, 0, 0);
+
+                option = {
+                    backgroundColor: {
+                        type: 'pattern',
+                        image: canvas,
+                        repeat: 'repeat'
+                    },
+                    tooltip: {},
+                    title: [{
+                        text: '资金分布（红色买入，黑色卖出）',
+                        subtext: '总计 ' + builderJson.all,
+                        x: '25%',
+                        textAlign: 'center'
+                    }, {
+                        text: '买入',
+                        subtext: '总计 ' + Object.keys(downloadJson).reduce(function (all, key) {
+                            return all + downloadJson[key];
+                        }, 0),
+                        x: '75%',
+                        textAlign: 'center'
+                    }, {
+                        text: '卖出',
+                        subtext: '总计 ' + Object.keys(themeJson).reduce(function (all, key) {
+                            return all + themeJson[key];
+                        }, 0),
+                        x: '75%',
+                        y: '50%',
+                        textAlign: 'center'
+                    }],
+                    grid: [{
+                        top: 50,
+                        width: '50%',
+                        bottom: '45%',
+                        left: 10,
+                        containLabel: true
+                    }, {
+                        top: '55%',
+                        width: '50%',
+                        bottom: 0,
+                        left: 10,
+                        containLabel: true
+                    }],
+                    xAxis: [{
+                        type: 'value',
+                        max: builderJson.all,
+                        splitLine: {
+                            show: false
+                        }
+                    }, {
+                        type: 'value',
+                        max: builderJson.all,
+                        gridIndex: 1,
+                        splitLine: {
+                            show: false
+                        }
+                    }],
+                    yAxis: [{
+                        type: 'category',
+                        data: Object.keys(builderJson.charts),
+                        axisLabel: {
+                            interval: 0,
+                            rotate: 30
+                        },
+                        splitLine: {
+                            show: false
+                        }
+                    }, {
+                        gridIndex: 1,
+                        type: 'category',
+                        data: Object.keys(builderJson.components),
+                        axisLabel: {
+                            interval: 0,
+                            rotate: 30
+                        },
+                        splitLine: {
+                            show: false
+                        }
+                    }],
+                    series: [{
+                        type: 'bar',
+                        stack: 'chart',
+                        z: 3,
+                        label: {
+                            normal: {
+                                position: 'right',
+                                show: true
+                            }
+                        },
+                        data: Object.keys(builderJson.charts).map(function (key) {
+                            return builderJson.charts[key];
+                        })
+                    }, {
+                        type: 'bar',
+                        stack: 'chart',
+                        silent: true,
+                        itemStyle: {
+                            normal: {
+                                color: '#eee'
+                            }
+                        },
+                        data: Object.keys(builderJson.charts).map(function (key) {
+                            return builderJson.all - builderJson.charts[key];
+                        })
+                    }, {
+                        type: 'bar',
+                        stack: 'component',
+                        xAxisIndex: 1,
+                        yAxisIndex: 1,
+                        z: 3,
+                        label: {
+                            normal: {
+                                position: 'right',
+                                show: true
+                            }
+                        },
+                        data: Object.keys(builderJson.components).map(function (key) {
+                            return builderJson.components[key];
+                        })
+                    }, {
+                        type: 'bar',
+                        stack: 'component',
+                        silent: true,
+                        xAxisIndex: 1,
+                        yAxisIndex: 1,
+                        itemStyle: {
+                            normal: {
+                                color: '#eee'
+                            }
+                        },
+                        data: Object.keys(builderJson.components).map(function (key) {
+                            return builderJson.all - builderJson.components[key];
+                        })
+                    }, {
+                        type: 'pie',
+                        radius: [0, '30%'],
+                        center: ['75%', '25%'],
+                        data: Object.keys(downloadJson).map(function (key) {
+                            return {
+                                name: key.replace('.js', ''),
+                                value: downloadJson[key]
+                            }
+                        })
+                    }, {
+                        type: 'pie',
+                        radius: [0, '30%'],
+                        center: ['75%', '75%'],
+                        data: Object.keys(themeJson).map(function (key) {
+                            return {
+                                name: key.replace('.js', ''),
+                                value: themeJson[key]
+                            }
+                        })
+                    }]
+                };
+                myChart.setOption(option);
+            }
+        },
+        error: function () {
+            alert("数据加载失败！请检查数据链接是否正确");
+        }
+    });
+}
